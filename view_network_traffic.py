9fff1bb1c8709c8f2c76b57963f47c85f3ef0e2c
George Pramantiotis <giorgospram@gmail.com>
2024-10-19 21:13:44 +0300

    removed vew network traffic

--- view_network_traffic.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('network_usage.db')
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM daily_usage')
-rows = cursor.fetchall()
-for row in rows:
-    print(f"Date: {row[0]}, Download: {row[1]} MB, Upload: {row[2]} MB")
-conn.close()
